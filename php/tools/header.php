94ad6731c29f05a17065bfd9e6d63b1894a64d47
Thiago <thiagozabala92@gmail.com>
2025-09-29 09:29:27 -0300

    FIX

--- php/tools/header.php
+++ php/tools/header.php
@@ -11,7 +11,7 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Hola</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Pricing</a>
